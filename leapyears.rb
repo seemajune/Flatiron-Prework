a394a6a4050c56dfb1254e5508c25ccee86d58a5
seemajune <seemashariat@gmail.com>
2014-08-29 15:10:17 -0400

    Update leapyears.rb

--- leapyears.rb
+++ leapyears.rb
@@ -10,23 +10,3 @@
 # has having July in the middle of the winter,
 # which is what would eventually happen.)
 
-def leapyears(startyear, endyear)
-	years = Array.new
-	# years {puts startyear.upto endyear}
-	years { |x| puts startyear + =1..endyear}
-	years.select { year % 4 == 0 && year % 400 == 0 }
-	puts years
-end
-
-
-
-puts 'Give me a starting year'
-
-startyear = gets.chomp
-startyear.to_i
-
-puts 'Give me an end year'
-endyear = gets.chomp
-endyear.to_i
-
-puts leapyears(startyear, endyear)
