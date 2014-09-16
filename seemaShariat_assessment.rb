3b5df17d065657006293a7f3a3b7c129c8d76e41
Selectable <selectable@Selectables-MacBook-Pro-2.local>
2014-09-16 19:52:10 -0400

    init add assessment file to git repo

---
+++ seemaShariat_assessment.rb
@@ -0,0 +1,18 @@
+#1. Arrays
+  array = ['Blake','Steven','Jeff']
+
+#a. Add a element to an array
+
+  array[3] = 'Seema'
+
+# b. Write a statement to print out all the elements of the array.
+
+  puts array
+
+# c. Return the value at index 1.
+ 
+  puts array[1]
+
+# d. Return the index for the value 'Jeff'.
+
+  puts array.index('Jeff')
